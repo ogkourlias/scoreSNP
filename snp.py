d2abf54e37bb32549a05ef823186ab0622917e65
ogkourlias <orfeasgkourlias@gmail.com>
2022-09-30 17:14:34 +0200

    Finished main script so that it may output a score. Documentation and readme still needs to be finished

--- snp.py
+++ snp.py
@@ -12,32 +12,76 @@ __version__ = "0.1"
 
 # IMPORTS
 import sys
-import biopython
+from Bio import AlignIO
 
 # CLASSES
 class aln_file():
     """ Creates object out of a provided ALN file. """
     def __init__(self, file):
-        self.file = open(file, "r")
-        self.raw = self.file.read()
+        self.file = AlignIO.read(sys.argv[1], "fasta")
 
     def __str__(self):
-        return self.raw
+        return self.file
+
 
 
 
 
 # FUNCTIONS
-def y():
+def get_counts(file):
     """ Desc """
-    return 0
+    pos_list = []
+    for pos in range(0, len((file[1].seq))):
+        snp_dict = {}
+        for aa in range(0,len(file)):
+            if file[aa, pos] == "-":
+                break
+            if file[aa, pos] not in snp_dict.keys():
+                snp_dict[file[aa, pos]] = 1
+            else:
+                snp_dict[file[aa, pos]] += 1
+        pos_list.append(snp_dict)
+    return pos_list
+
+def count_conflicts(pos_list):
+    """ Desc """
+    classi_conflict = 0
+
+    for dict in pos_list:
+        aa_clases = {
+            "GAVLI": 0,
+            "SCUTM": 0,
+            "P": 0,
+            "FYW": 0,
+            "HKR": 0,
+            "DENQ": 0
+        }
+        if len(dict) > 1:
+            for aa in dict:
+                for classi in aa_clases:
+                    if aa in classi:
+                        aa_clases[classi] = 1
+
+            if sum(aa_clases.values()) > 1:
+                classi_conflict += 1
+
+    return classi_conflict
+
+def rate_harm(conflict_count, file):
+    """ Desc """
+    ratio = conflict_count / len(file[1].seq)
+
+    return f"The MSA receives a conservation score of {round(10-ratio*10)}, " \
+           f"because {round(ratio*100)}% of the sequence positions contain an AA class conflict."
 
 
 # MAIN
 def main(args):
     """ Main function """
-    main_file = aln_file(sys.argv[1])
-    print(main_file)
+    file = AlignIO.read(sys.argv[1], "fasta")
+    pos_list = get_counts(file)
+    conflict_count = count_conflicts(pos_list)
+    print(rate_harm(conflict_count, file))
     # FINISH
     return 0
 
